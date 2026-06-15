1f73215ca7f0b8318dc54cf25d4954e54bff4b27
aiminnano <gabriellesmateo@gmail.com>
2026-06-15 15:41:48 +0800

    adjusted the design for the newly added components

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,15 @@
 import PersonalInfo from "./PersonalInfo";
 import Steps from "./Steps";
+import SelectPlan from "./SelectPlan";
 
 function Container() {
     return (
         <>
             <div className="relative grid md:place-items-center min-h-screen font-ubuntu box-border">
-                <div className=" flex flex-col md:flex-row items-center md:items-start px-3 rounded-2xl bg-blue100 md:bg-white md:p-4.5 md:pr-0 w-full max-w-[940px]">
+                <div className=" flex flex-col md:flex-row items-center md:items-start px-4 rounded-2xl bg-blue100 md:bg-white md:p-4.5 md:pr-0 w-full max-w-[940px] md:shadow-xl">
                     <Steps />
                     <PersonalInfo />
+                    <SelectPlan />
                 </div>
             </div>
         </>

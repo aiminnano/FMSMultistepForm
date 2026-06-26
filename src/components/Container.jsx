637fcd13407fc1dd42483c010bc9bd1243bc56a8
aiminnano <gabriellesmateo@gmail.com>
2026-06-26 17:30:51 +0800

    added more functionality

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -19,7 +19,7 @@ function Container() {
     return (
         <>
             <div className="relative grid md:place-items-center min-h-screen font-ubuntu box-border">
-                <div className=" flex flex-col md:flex-row items-center md:items-start px-4 rounded-2xl bg-blue100 md:bg-white md:p-4.5 md:pr-0 w-full max-w-[940px] md:shadow-xl">
+                <div className="relative flex flex-col md:flex-row items-center md:items-start px-4 rounded-2xl bg-blue100 md:bg-white md:p-4.5 md:pr-0 w-full max-w-[940px] md:shadow-xl">
                     <Steps step={step} />
                     <PersonalInfo step={step} setStep={setStep} />
                     <SelectPlan step={step} setStep={setStep} option={option} setOption={setOption} plan={plan} setPlan={setPlan} isOn={isOn} setIsOn={setIsOn}/>
--- src/components/FinishingUp.jsx
+++ src/components/FinishingUp.jsx
@@ -4,7 +4,7 @@ function FinishingUp({step, setStep, option, setOption, plan, setPlan, isOn, onl
     return(
         <>
             <div className={`${step === 4 ? 'flex' : 'hidden'} flex-col gap-30 md:gap-20 w-full`}>
-                <div className="relative z-1 flex flex-col items-start shadow-2xl md:shadow-none rounded-xl gap-5 md:gap-6 w-full max-w-[768px] p-6 pt-8 pb-8 md:p-10 md:pl-25 md:pr-25 md:max-w-[643px] bg-white">
+                <div className=" z-1 flex flex-col items-start shadow-2xl md:shadow-none rounded-xl gap-5 md:gap-6 w-full max-w-[768px] p-6 pt-8 pb-8 md:p-10 md:pl-25 md:pr-25 md:max-w-[643px] bg-white">
                     <div className="flex flex-col items-start gap-2 md:gap-1">
                         <h1 className="text-2xl md:text-[33px] font-bold text-blue950">Finishing up</h1>
                         <p className="text-grey500">Double-check everything looks OK before confirming.</p>
@@ -17,26 +17,31 @@ function FinishingUp({step, setStep, option, setOption, plan, setPlan, isOn, onl
                             </div>
                             <div className="text-blue950 font-bold">$9/mo</div>
                         </div>
-                        <div className="flex items-center place-content-between w-full md:pt-1">
+                        {onlineService && <div className="flex items-center place-content-between w-full md:pt-1">
                             <div>   
                                 <div className="text-grey500 text-[15px] ">Online service</div>
                             </div>
-                            <div className="text-blue950">+$1/mo</div>
-                        </div>
-                        <div className="flex items-center place-content-between w-full ">
-                            <div>
-                                
+                            <div className="text-blue950">{isOn ? '+$10/yr' : '+$1/mo'}</div>
+                        </div>}
+                        {largerStorage && <div className="flex items-center place-content-between w-full ">
+                            <div>    
                                 <div className="text-grey500 text-[15px] ">Larger storage</div>
                             </div>
-                            <div className="text-blue950 ">+$2/mo</div>
-                        </div>
+                            <div className="text-blue950 ">{isOn ? '+$20/yr' : '+$2/mo'}</div>
+                        </div>}
+                        {customProfile && <div className="flex items-center place-content-between w-full ">
+                            <div>    
+                                <div className="text-grey500 text-[15px] ">Customizable Profile</div>
+                            </div>
+                            <div className="text-blue950 ">{isOn ? '+$20/yr' : '+$2/mo'}</div>
+                        </div>}
                     </div>
                     <div className="flex items-center place-content-between w-full p-4 pb-0 pt-0">
                         <div className="text-grey500 text-[15px]">Total (per month)</div>
                         <div className="text-purple600 font-bold text-xl">+$12/mo</div>
                     </div>
                 </div>
-                <div className="flex items-center place-content-between md:pl-25 bg-white w-full p-4 md:p-0 md:pr-25 fixed bottom-0 left-0 right-0 md:relative md:bottom-auto md:left-auto md:right-auto ">
+                <div className="z-2 flex items-center place-content-between md:pl-25 bg-white w-full p-4 md:p-0 md:pr-25 fixed bottom-0 left-0 right-0 md:absolute md:bottom-8 md:left-auto md:right-auto md:max-w-[648px] ">
                     <div className='cursor-pointer text-grey500 text-sm hover:text-blue950 font-medium md:text-base' onClick={() => setStep(3)}>Go back</div>
                     <button type="button" className=" hover:bg-blue300 bg-blue950 text-white text-sm p-3 md:pl-6 md:pr-6 rounded-md md:text-base cursor-pointer md:bg-purple600">Confirm</button>
                 </div>
